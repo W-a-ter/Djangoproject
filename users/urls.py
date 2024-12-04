0784924b42b7d86c5a5995cd61c98aa51849d40f
verba4182 <verba4182@mail.ru>
2024-12-04 11:12:18 +0300

    04/12 fixed code

--- users/urls.py
+++ users/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from users.apps import UsersConfig
 from django.contrib.auth.views import LoginView, LogoutView
-from users.views import UserRegisterView, email_verification
+from users.views import UserCreateView, email_verification
 
 
 app_name = UsersConfig.name
@@ -9,6 +9,6 @@ app_name = UsersConfig.name
 urlpatterns = [
     path('login/', LoginView.as_view(template_name="login.html"), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
-    path('register/', UserRegisterView.as_view(), name='register'),
+    path('register/', UserCreateView.as_view(), name='register'),
     path('email-confirm/<str:token>/', email_verification, name='email-confirm')
 ]
--- users/views.py
+++ users/views.py
@@ -11,8 +11,9 @@ from .forms import UserRegisterForm
 from config.settings import EMAIL_HOST_USER
 
 
-class UserRegisterView(CreateView):
-    template_name = 'users/register.html'
+class UserCreateView(CreateView):
+    model = User
+    #template_name = 'users/register.html'
     form_class = UserRegisterForm
     success_url = reverse_lazy('user:login')
 
